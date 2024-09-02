79800259f234ca8b5f85646179c73d8154843cb8
Shah Ehsan <shahehsan2002@gmail.com>
2024-09-03 02:55:50 +0600

    Project setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <Button className="text-center " >My TODO</Button>
+    </>
+  )
+}
+
+export default App
